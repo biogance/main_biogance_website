12d9453dce24f2c7be303d1ebfcea2ea6c22a13e
Ahmad <ahmad@drudotstech.com>
2026-05-13 17:50:42 +0500

    Applied API's on pro section in Distributor, Reseller and In Ambassder and also add API in certifcate and who are we sections

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -55,7 +55,7 @@ export default function RootLayout({ children }) {
           <I18nProvider>
             <PageLoader />
             <Suspense fallback={null}><RouteTopLoader /></Suspense>
-            <Toaster position="top-right" />
+            <Toaster position="top-right" toastOptions={{ style: { zIndex: 99999 } }} containerStyle={{ zIndex: 99999 }} />
             {children}
           </I18nProvider>
         </ReduxProvider>
